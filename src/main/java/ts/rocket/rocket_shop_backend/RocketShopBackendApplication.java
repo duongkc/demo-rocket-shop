f9d93e6af318a0eb8ce2d118fd0ec13316fa7cff
KC <kimchauduong@gmail.com>
2021-10-13 10:35:37 +0200

    Initial commit

---
+++ src/main/java/ts/rocket/rocket_shop_backend/RocketShopBackendApplication.java
@@ -0,0 +1,13 @@
+package ts.rocket.rocket_shop_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketShopBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ts/rocket/rocket_shop_backend/RocketShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ts.rocket.rocket_shop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
